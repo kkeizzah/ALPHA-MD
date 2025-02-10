92d18aa1c18d53a846377456a090c60972be4101
kkeizzah <keizzahketh@gmail.com>
2025-02-10 09:30:38 +0300

    Update System.js

--- commands/System.js
+++ commands/System.js
@@ -367,7 +367,7 @@ keith({
         `https://api.heroku.com/apps/${herokuAppName}/builds`,
         {
           source_blob: {
-            url: "https://github.com/Keithkeizzah/ALPHA-MD/tarball/main",
+            url: "https://github.com/kkeizzah/ALPHA-MD/tarball/main",
           },
         },
         {
